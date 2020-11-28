a6b977de4c5b2924f2d42471820295bc7de1aced
leo <Angeleo4869@163.com>
2020-11-28 15:56:18 +0800

    利用字符实现字符串修改方法

--- Demo01/BYTE_RUNE/main.go
+++ Demo01/BYTE_RUNE/main.go
@@ -1,5 +1,5 @@
 package main
-
+import "strings"
 import "fmt"
 //byte 和 rune 类型
 func main() {
@@ -7,4 +7,13 @@ func main() {
 	b := '字' 
 	fmt.Printf("%T + %v + %c \n",a , a , a)
 	fmt.Printf("%T + %c \n",b , b)
+
+	//字符操作字符串
+	hello := "Hello World !"
+	var hellos  = strings.Split(hello," ")
+	fmt.Printf("Hello: %v\n",hello)
+	fmt.Printf("Hellos: %v\n", hellos)
+	hellos[0] = "你好"//修改字符串
+	// hello[0] = '你' //编译异常 cannot assign to hello[0]
+	fmt.Printf("Hellos: %s \n",hellos)
 }
