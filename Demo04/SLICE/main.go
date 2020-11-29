0d38620274eec327535756c438048143349899cf
leo <Angeleo4869@163.com>
2020-11-30 00:56:58 +0800

    切片的比较 切片的遍历

--- Demo04/SLICE/main.go
+++ Demo04/SLICE/main.go
@@ -15,7 +15,11 @@ func main() {
 
 	c := array[1:5]//给予一个数组切割，左闭右开
 	fmt.Println(c,len(c),cap(c))//初始值为[2 3 4 5] ，长度为4,容量为6
+	d := c[2:] //二次切片
+	fmt.Println(d,len(d),cap(d))//初始值为[4 5] ，长度为2,容量为4
 
-
+	//make函数创建切片
+	e := make([]int ,4 , 6)
+	fmt.Println(e,len(e),cap(e))//初始值为[0 0 0 0]，长度为 4，容量为 6
 
 }
