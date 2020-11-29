e239125618fbfd14e3e3ddb794c710dc4b65d50f
leo <Angeleo4869@163.com>
2020-11-29 10:13:12 +0800

    switch分支

---
+++ Demo02/SWITCH_CASE/main.go
@@ -0,0 +1,28 @@
+package main
+
+//switch - case 结构
+import "fmt"
+
+func main() {
+	finger := 3
+	switch finger{
+	case 1:{
+		fmt.Println("Finger: ",finger)
+	}
+	case 2:{
+		fmt.Println("Finger: ",finger)
+	}
+	case 3:{
+		fmt.Println("Finger: ",finger)
+	}
+	case 4:{
+		fmt.Println("Finger: ",finger)
+	}
+	case 5:{
+		fmt.Println("Finger: ",finger)
+	}
+	default :{
+		fmt.Println("Finger: ",finger)
+	}
+	}
+}
