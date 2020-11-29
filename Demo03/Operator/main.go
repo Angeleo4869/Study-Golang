d6b778cc9c82fe25bb0d9d4bc0e1809d10e3750c
leo <Angeleo4869@163.com>
2020-11-29 14:26:25 +0800

    运算符

---
+++ Demo03/Operator/main.go
@@ -0,0 +1,35 @@
+package main
+
+//运算符
+import "fmt"
+
+func main() {
+
+	const PI = 3.14
+	a := 3
+	b := 4
+	//算术运算符
+	fmt.Println(a + b)
+	fmt.Println(b - a)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
+	// var d float = 2 * a * PI //Go语言中，int 类型不能自动转型为 float 类型
+	a++//自增1
+	b--//自减1
+	fmt.Println(a) 
+	fmt.Println(b) 
+	// fmt.Println(a++)//单目运算符不能作为Println参数
+	//比较运算符
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+	fmt.Println(a > b)
+	fmt.Println(a < b)
+	fmt.Println(a >= b)
+	fmt.Println(a <= b)
+	//逻辑运算符
+	fmt.Println( a > 3 && b < 4)
+	fmt.Println( a > 3 || b < 4)
+	fmt.Println( ! (a > 3 ))
+
+}
